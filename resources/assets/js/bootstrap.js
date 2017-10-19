c321fd0e285a1ab8b6c0c887384e76eb1022dbeb
HoangLN <hoang.ln@neo-lab.vn>
2017-10-19 09:08:46 +0700

    vue route

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+
+window.Vue = Vue;
+Vue.use(VueRouter);
+
+window.axios = axios;
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest'
+};
